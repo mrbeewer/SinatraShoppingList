f0aed35047ebfe3a2565703cfd5df77b44ba8a03
David Beeler <david.beeler@gmail.com>
2015-11-24 11:07:59 -0600

    intial push

---
+++ app.rb
@@ -0,0 +1,26 @@
+require "bundler"
+Bundler.require
+
+get "/" do
+
+  erb :home
+
+end
+
+get "/home" do
+
+  erb :home
+
+end
+
+get "/login" do
+
+  erb :login
+
+end
+
+get "/registration" do
+
+  erb :registration
+
+end
